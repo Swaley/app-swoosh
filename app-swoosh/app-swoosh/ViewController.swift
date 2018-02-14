0f38cd4a05e5e6ff2441d4b6065bb22e063af540
Swaley Palmer <swaley1980@swaleys-macbook-pro.local>
2018-02-14 11:13:37 +0000

    alignment of swoosh

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -17,11 +17,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //centering swoosh logo
-        swoosh.frame = CGRect(x: view.frame.size .width / 2 - swoosh.frame.size.width / 2,  y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
-        
-        bgImg.frame = view.frame;
-        
     }
     
     
